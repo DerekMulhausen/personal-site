0892ec5b41631d9bff46f605a968bb2130185d0a
dmulhausen <westbycomputers@hotmail.com>
2022-09-13 16:45:40 -0500

    updated experience

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './styles/styles.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
+import './scripts/populateSkills';
 import Header from './siteComponents/header';
-import 
+import popSkills from './scripts/populateSkills';
+
 
 function App() {
+  //var exp = JSON.parse(fetch('../data/experience.json'));
+  //var skills = JSON.parse(fetch('../data/skills.json'));
+//  popSkills(); 
   return (
     <>
+
       <Header />
     </>
   );
